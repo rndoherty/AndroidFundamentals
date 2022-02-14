f3f380030dcb1ef4cfa09f6eaccdce09becb45ea
Ryan Doherty <rdoherty@csumb.edu>
2022-02-13 17:52:33 -0800

    Added log message

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "Hello World")
+
+
+
+
+
+
+
+
     }
 }
