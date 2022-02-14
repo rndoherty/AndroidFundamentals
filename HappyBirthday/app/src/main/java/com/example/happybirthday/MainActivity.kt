23ee357aadc0de3d34614626bc75dc877e1258d2
Ryan Doherty <rdoherty@csumb.edu>
2022-02-13 18:27:42 -0800

    Added button that sends a log message

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.happybirthday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    
+
+    fun sendLogMessage(view: View){
+        Log.d("MainActivity", "You pressed me!")
+    }
 
 }
