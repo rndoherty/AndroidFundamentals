5b8f53ab1151f0c415fd45d727b17b2fa0c88d0e
Ryan Doherty <rdoherty@csumb.edu>
2022-02-13 19:44:17 -0800

    Added first Activity with Send button

--- TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.kt
+++ TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.twoactivities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val LOG_TAG = MainActivity::class.qualifiedName
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun launchSecondActivity(view: View) {
+        Log.d(LOG_TAG, "Button Clicked!")
+
     }
 }
