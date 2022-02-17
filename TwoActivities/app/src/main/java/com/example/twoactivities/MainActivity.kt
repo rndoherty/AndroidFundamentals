d5c2e40004108e8170f5d2e83f6a1b87be5e64d4
Ryan Doherty <rdoherty@csumb.edu>
2022-02-17 11:36:17 -0800

    2.1 Task 3: Send user input text to SecondActivity

--- TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.kt
+++ TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.kt
@@ -4,22 +4,33 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
         private val LOG_TAG = MainActivity::class.qualifiedName
+        const val EXTRA_MESSAGE =
+            "com.example.android.twoactivities.extra.MESSAGE"
+
     }
+
+    private lateinit var mMessageEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mMessageEditText = findViewById(R.id.editText_main)
+
+
     }
 
     fun launchSecondActivity(view: View) {
         Log.d(LOG_TAG, "Button Clicked!")
         val intent = Intent(this, SecondActivity::class.java)
+        val message = mMessageEditText.getText().toString()
+        intent.putExtra(EXTRA_MESSAGE, message)
         startActivity(intent)
     }
 }
--- TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.kt
+++ TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.kt
@@ -1,11 +1,27 @@
 package com.example.twoactivities
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity() {
+
+
+    companion object {
+        private val LOG_TAG = SecondActivity::class.qualifiedName
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val intent = this.intent
+        val message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE).toString()
+        val textView: TextView = findViewById(R.id.text_message)
+        Log.d(SecondActivity.LOG_TAG, message)
+        textView.text = message
+
+
     }
 }
